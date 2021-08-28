c5b1a6ee296dfd6bba9d1a0d7de6a2676cffeaa5
pablo14simon <88359228+pablo14simon@users.noreply.github.com>
2021-08-27 22:06:54 -0400

    Add files via upload

---
+++ P05_llena.py
@@ -0,0 +1,50 @@
+import numpy as np
+from numpy import float64
+from laplaciana import laplaciana
+from time import perf_counter
+
+def matriz_laplaciana(M, t=np.float64):
+    e=np.eye(M)-np.eye(M,M,1)
+    return t(e+e.T)
+
+
+
+ensamblaje=[]
+calculo=[]
+tamaño=[]
+
+i=0
+while i<10:
+    M=[2,3,5,7,10,12,15,20,30,40,50, 70, 100, 150, 200, 300, 500, 750, 1000, 1800, 2500, 4000]
+    for j in M:     
+        t1 = perf_counter()
+        A=laplaciana(j, dtype=float64)
+        B=laplaciana(j, dtype=float64)
+        
+        t2 = perf_counter()
+        dt_ensamblaje = t2-t1
+        
+        t3=perf_counter()
+        C=A@B
+        
+        t4=perf_counter()
+        dt_calculo = t4-t3
+        
+        ensamblaje.append(dt_ensamblaje)
+        calculo.append(dt_calculo)
+        tamaño.append(j)
+        
+        print(f"dt_calculo = {dt_calculo} s")
+        print(f"dt_ensamblaje = {dt_ensamblaje} s")
+        print (f"tamaño M = {j}")
+        print ()
+    i+=1
+
+arch = open("Caso_1_llena.txt", "w")
+x = 0
+arch.write("Tiempo de ensamblaje(s)     Tiempo en calcular(s)     Tamaño matriz \n")
+while x < len(ensamblaje):
+    arch.write(f"{ensamblaje[x]}   {calculo[x]}   {tamaño[x]} \n")
+    x+=1
+    
+arch.close()
